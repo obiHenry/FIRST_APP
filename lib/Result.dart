a7f4e83e8ae5a7443ba7b529f3792d06482181f7
Element Obi <obihenry578@gmail.com>
2020-08-18 07:34:08 -0700

    modified result and question files

--- lib/Result.dart
+++ lib/Result.dart
@@ -7,7 +7,6 @@ class Result extends StatelessWidget {
   Result(this.resultScore, this.resetHandler);
 
   String get resultPhrase {
-
     String resultText;
     if (resultScore <= 10) {
       resultText = 'improve brother man';
@@ -16,23 +15,46 @@ class Result extends StatelessWidget {
     } else if (resultScore <= 30) {
       resultText = 'more greese to ur elbow';
     } else {
-      resultText = ' where do u belong. ask ur self that question and get back to me';
+      resultText =
+          ' where do u belong. ask ur self that question and get back to me';
     }
     return resultText;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          Text(
-            resultPhrase,
-            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-            textAlign: TextAlign.center, 
-          ),
-          FlatButton(onPressed:resetHandler , child: Text('Restart Quiz'),textColor: Colors.blue,)
-        ],
+    return Container(
+      child: Center(
+        child: Column(
+          children: [
+            Text(
+              
+              resultPhrase,
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            
+            FlatButton(
+              onPressed: resetHandler,
+              child: Text('Restart Quiz', style: TextStyle(fontSize: 20),),
+              textColor: Colors.blue,
+              
+            ),
+
+           
+              Container(
+               
+                child: Text(
+                  
+                  '$resultScore', 
+                   style: TextStyle(fontSize: 40, color: Colors.blue), 
+                 ),
+              ) 
+            
+              ,
+          
+          ],
+        ),
       ),
     );
   }
